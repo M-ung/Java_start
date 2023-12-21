ebae4e77a9bb5c10d7d8bcaea4a7ba2c03470cb5
김멍몽 <wjdahrrla@gmail.com>
2023-12-21 13:52:17 +0900

    [202312211352]

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) { // psvm 엔터를 입력하면 자동 작성이 된다.
+        // sout 엔터를 입력하면 자동 작성이 된다.
+        System.out.println("hello java1"); // hello java1을 출력한다.
+//        System.out.println("hello java2");
+        System.out.println("hello java3");
+        /*
+           System.out.println("hello java4");
+           System.out.println("hello java5");
+         */
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    public static void main(String[] args) { // 프로그램을 실행하는 시작점
+        System.out.println("hello java");
+    }
+}
+
+// 자바 언어는 대소문자를 구분하기 때문에 주의해야 한다!!
